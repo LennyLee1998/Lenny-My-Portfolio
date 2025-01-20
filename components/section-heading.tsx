a83775804f1d7ff796950fda4186b59a8a3b5f28
Lenny <ly2484579947@gmail.com>
2025-01-20 15:22:20 +0800

    feat: add `section-heading` component

--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -1,9 +1,13 @@
 import React from "react";
-interface SectionHeadingProps {
+type SectionHeadingProps = {
   // Represents all of the things React can render.
   children: React.ReactNode;
-}
+};
 // heading title component
 export default function SectionHeading({ children }: SectionHeadingProps) {
-  return <h2 className="text-center text-3xl font-medium capitalize mb-8">{children}</h2>;
+  return (
+    <h2 className="text-center text-3xl font-medium capitalize mb-8">
+      {children}
+    </h2>
+  );
 }
